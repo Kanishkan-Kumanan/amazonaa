2bf2e447e4ee541107e52965b8167dfe7345c7e2
Kanishkan <kanishk.aero@gmail.com>
2023-05-04 23:12:48 +0530

    third commit

--- frontend-app/src/App.js
+++ frontend-app/src/App.js
@@ -1,4 +1,4 @@
-
+import data from "./data";
 function App() {
   return (
     <div>
@@ -6,6 +6,15 @@ function App() {
         <a href='/'>Amazonaa</a>
       </header>
       <main>Listing</main>
+      {
+        data.products.map(product =>(
+          <div>
+            <img src={product.image} alt={product.image}/>
+            <p>{product.name}</p>
+            <p>{product.price}</p>
+          </div>
+        ))
+      }
     </div>
   );
 }
